0671085917208807e81d33e0cf7991d341a60d88
Nabin Budhathoki <tusharbcbudhathoki@gmail.com>
2025-12-02 20:51:49 +0545

    fixed social links

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -21,27 +21,27 @@ const Contact = () => {
   }
   return (
     <>
-    <div className='flex flex-col justify-center items-center h-screen min-w-screen'>
+    <div className='flex flex-col justify-center items-center h-screen min-w-screen overflow-x-hidden'>
     <Navbar />
-     <div className='h-[35%] flex justify-center items-center'>
+     <div className='h-[30%] flex justify-center items-center'>
       <p className='text-muted text-neon text-center w-[60%]'>Got a question, proposal or project or want to work together on something? Feel free to reach out.</p>
      </div>
-     <div className="relative gmail text-center h-[70%] ">
+     <div className="relative gmail text-center h-[20%] ">
      <Separator/>
       <p className='text-2xl md:text-6xl text-neon m-20 cursor-pointer'
       onMouseEnter={()=> setIsHover(true)}
       onMouseLeave={()=> setIsHover(false)}
       onClick={()=> handleCopy()}
       >nabinbudhathoki2004@gmail.com</p>
-      { isHover && <div className='absolute top-40 left-[700px]'><PrimaryButton btnText={showCopy}/></div>}
+      { isHover && <div className='absolute top-35 left-[220px] md:top-40 md:left-[700px]'><PrimaryButton btnText={showCopy}/></div>}
      <Separator />
      </div>
-     <div className="social flex justify-center items-center">
-     <div className="buttons-wrapper flex justify-center items-end h-full">
-     <PrimaryButton btnText="Linkedn"/>
-      <PrimaryButton btnText="Github"/>
-      <PrimaryButton btnText="Facebook"/>
-      <PrimaryButton btnText="Instagram"/>
+     <div className="social flex justify-center items-center h-[50%]">
+     <div className="buttons-wrapper flex md:justify-center md:items-end">
+     <PrimaryButton btnText="Linkedn" href='https://www.linkedin.com/in/budhathoki-nabin/'/>
+      <PrimaryButton btnText="Github" href='https://github.com/Nabin0224'/>
+      <PrimaryButton btnText="Facebook" href='https://www.facebook.com/nabin.budhathoki.196568'/>
+      <PrimaryButton btnText="Instagram" href='https://www.instagram.com/nabin_0224?igsh=ZXV2M243YWlsZHox&utm_source=qr'/>
      </div>
      </div>
     </div>
--- components/ui/PrimaryButton.tsx
+++ components/ui/PrimaryButton.tsx
@@ -3,16 +3,25 @@ import playAudio from '@/app/lib/playAudio'
 import React from 'react'
 
 interface ButtonProps {
-  btnText: String
+  btnText: string
+  href?: string     
 }
-const PrimaryButton = ({btnText}: ButtonProps ) => {
+
+const PrimaryButton = ({ btnText, href }: ButtonProps) => {
+  const handleClick = () => {
+    if (href) {
+      window.open(href, "_blank")   
+    }
+  }
+
   return (
     <button
-    className="rounded-2xl outline-gray-700 outline-1 hover:ring-[3px] hover:text-neon hover:text-primary hover:ring-[var(--color-primary)] hover:shadow-[0_0_15px_4px_var(--color-primary)] px-4 py-2 m-1 text-sm transition-all duration-300 "
-  onMouseEnter={playAudio}
-  >
-    <span className=''>{btnText}</span>
-  </button>
+      onClick={handleClick}
+      onMouseEnter={playAudio}
+      className="rounded-2xl outline-gray-700 outline-1 hover:ring-[3px] hover:text-neon hover:text-primary hover:ring-[var(--color-primary)] hover:shadow-[0_0_15px_4px_var(--color-primary)] px-4 py-2 m-1 text-sm transition-all duration-300"
+    >
+      <span>{btnText}</span>
+    </button>
   )
 }
 
