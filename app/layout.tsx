5e905e48e9962da9534cfc1d1c54efb5fa52626d
Nabin Budhathoki <tusharbcbudhathoki@gmail.com>
2025-12-02 22:26:04 +0545

    added meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,9 +13,35 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "Nabin Budhathoki",
-  description: "I am a Full Stack Developer based in Nepal, Kathmandu",
+  title: "Nabin Budhathoki | Full Stack Developer",
+  description: "Full Stack Developer based in Kathmandu, Nepal. MERN stack, Next.js, Tailwind CSS, and modern UI development.",
+
+  openGraph: {
+    title: "Nabin Budhathoki | Portfolio",
+    description:
+      "Explore my developer portfolio with MERN stack projects, UI/UX work, and full-stack apps.",
+    url: "https://myportfolio-iota-inky.vercel.app/",   
+    siteName: "Nabin Budhathoki Portfolio",
+    images: [
+      {
+        url: "https://myportfolio-iota-inky.vercel.app/metaImg.png", 
+        width: 1200,
+        height: 630,
+        alt: "Nabin Budhathoki Portfolio Preview",
+      },
+    ],
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Nabin Budhathoki | Portfolio",
+    description:
+      "Explore MERN stack projects, full-stack apps, and modern UI development.",
+    images: ["https://myportfolio-iota-inky.vercel.app/metaImg.png"],
+  },
 };
+
 
 export default function RootLayout({
   children,
